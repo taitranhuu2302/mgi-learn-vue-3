d5e798ddc85d579793dd3935a67fb35c18124173
taitranhuu2302 <taitranhuu2002@gmail.com>
2023-05-23 14:18:17 +0700

    update

--- src/directives/ClickOutside.ts
+++ src/directives/ClickOutside.ts
@@ -3,10 +3,8 @@ import type { Directive } from 'vue'
 const clickOutSide: Directive = {
   beforeMount: (el, binding) => {
     el.clickOutside = (event: MouseEvent) => {
-      if (event.target !== el && !el.contains(event.target)) {
-        if (binding.value instanceof Function) {
-          binding.value()
-        }
+      if (event.target !== el && !el.contains(event.target) && binding.value instanceof Function) {
+        binding.value()
       }
     }
 
